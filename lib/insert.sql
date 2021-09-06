8b09848de46d875c4fa2852aca8f3b1f218216cb
Nancy Zaky <nancyzki@yahoo.com>
2021-09-06 14:01:00 -0700

    commit tree 2d4b4eb79585d18c456b1654cb6533f99b1b3de2

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 3, "M");
+
+
+INSERT INTO bears (name, age, sex) VALUES ("Rowdy", 2, "M");
+
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 8, "M");
+
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 4, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 6, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 15, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 15, "F");
+INSERT INTO bears (name, age, sex) VALUES (null,89, "F");
